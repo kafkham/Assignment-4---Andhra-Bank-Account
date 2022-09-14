8f2372656c541b25503b06f75c5dd1be98c57130
kasraafkham <kasraafkham@gmail.com>
2022-09-13 21:17:19 -0400

    specify ignore files and initialize repository

---
+++ src/AndhraBankAccount.java
@@ -0,0 +1,130 @@
+//Kasra Afkham-Ebrahimi, McGill ID #260517717
+
+import java.time.LocalDateTime;
+
+public class AndhraBankAccount {
+
+    private static double annualInterestRate;
+    private static int nextAccountNb = 300;
+    private final int accountNb;
+    private String f_name;
+    private String l_name;
+    private double balance;
+    private final LocalDateTime dateCreated;
+
+    // account constructor
+    public AndhraBankAccount(String f_name, String l_name, double balance) {
+        this.accountNb = nextAccountNb;
+        nextAccountNb++; // add 1 so the next account number can be properly assigned
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.balance = balance;
+        this.dateCreated = LocalDateTime.now();
+    }
+
+    public static double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public static void setAnnualInterestRate(double annualInterestRate) {
+        AndhraBankAccount.annualInterestRate = annualInterestRate;
+    }
+
+    public int getAccountNb() {
+        return accountNb;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return getAnnualInterestRate() / 12;
+    }
+
+    public void withdraw(double withdrawAmount) {
+        //subtract withdrawal amount from balance and set the result as the new balance
+        setBalance(getBalance() - withdrawAmount);
+    }
+
+    public void deposit(double depositAmount) {
+        //add deposit amount to balance and set the result as the new balance
+        setBalance(getBalance() + depositAmount);
+    }
+
+    public boolean nameChecker(String name) {
+        //Name must contain at least one character. The first character must be alphanumeric.
+        char c = name.charAt(0);
+        if (!isALetter(c)) {
+            System.out.println("Error: invalid name");
+            return false;
+        }
+        //loop will verify each char
+        for (int i = 0; i < name.length(); i++) {
+            c = name.charAt(i);
+            //special statement for the final character of the prefix, since d would otherwise be out of range
+            if (i == name.length() - 1) {
+                //return true if the final character is alphanumeric and is not a special character
+                boolean b = isALetter(c) && ((c != ' ') && (c != '-'));
+                if (b) return true;
+                else {
+                    System.out.println("Error: invalid name");
+                    return false;
+                }
+            }
+            /*
+            name should contain only alphanumeric characters, spaces (‘ ’) or dashes (‘-’)
+            a space or a dash must always be followed by one or more alphanumeric characters
+
+            (!isValidChar(c)): calls ValidChar method to verify whether c is valid
+            if c is not valid, the method will return false and the expression will return true
+            then the if statement will execute and the overall method (isValidPrefix) will return false
+
+            ((c == ' ') || (c == '-')): verifies whether (c) is one of the special characters
+            !isAlphanumeric(d): calls isAlphanumeric method to verify if (d), the character after (c), is alphanumeric
+            if both statements above return true (i.e. (c) is a special character and (d) is not alphanumeric)
+            then the if statement will execute and the overall method (isValidPrefix) will return false
+            */
+            char d = name.charAt(i + 1);
+            if ((!isValidChar(c)) || (((c == ' ') || (c == '-')) && !isALetter(d))) {
+                System.out.println("Error: invalid name");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isALetter(char aC) {
+        //return true if c is a letter
+        return (Character.isLetter(aC));
+    }
+
+    public static boolean isValidChar(char pC) {
+        //return true if c is a letter, a space or a dash
+        return (isALetter(pC) || pC == ' ' || pC == '-');
+    }
+}
---
+++ src/AndhraBankAccountDriver.java
@@ -0,0 +1,100 @@
+//Kasra Afkham-Ebrahimi, McGill ID #260517717
+
+import java.util.Scanner;
+
+public class AndhraBankAccountDriver {
+    public static void main(String[] args) {
+
+        AndhraBankAccount.setAnnualInterestRate(4.5);
+
+        //welcome message
+        System.out.println("Welcome to ANDHRA Bank\n\nToday's Interest Rate is = "
+                + AndhraBankAccount.getAnnualInterestRate() + "%\n");
+
+        AndhraBankAccount account = new AndhraBankAccount("", "", 100);
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean firstNameCheck = false;
+        boolean lastNameCheck = false;
+
+        //prompt user for first name until they enter a valid response
+        while (!firstNameCheck) {
+            System.out.print("Enter first name: ");
+            String F_name = scanner.nextLine();
+            firstNameCheck = account.nameChecker(F_name);
+            if (firstNameCheck) account.setF_name(F_name);
+        }
+        //prompt user for last name until they enter a valid response
+        while (!lastNameCheck) {
+            System.out.print("Enter last name: ");
+            String L_name = scanner.nextLine();
+            lastNameCheck = account.nameChecker(L_name);
+            if (lastNameCheck) account.setL_name(L_name);
+        }
+
+        //display newly created account details
+        System.out.println("\nYour account details are:" +
+                "\nName is: " + account.getF_name() + " " + account.getL_name() +
+                "\nAccount Number is " + account.getAccountNb() +
+                "\nCurrent balance is $" + account.getBalance() +
+                "\nInterest Rate is = " + AndhraBankAccount.getAnnualInterestRate() + "%" +
+                "\nMonthly interest for this balance is $" + account.getBalance() * 0.01 * account.getMonthlyInterestRate() +
+                "\nThis account was created on " + account.getDateCreated());
+
+        String userChoice = "";
+
+        // ask for, validate, and execute user-selected action
+        while (!userChoice.equals("2")) {
+            scanner = new Scanner(System.in);
+            System.out.print("\nEnter your choice (0: deposit, 1: withdraw, 2: quit): ");
+            userChoice = scanner.nextLine();
+            switch (userChoice) {
+
+                //deposit
+                case "0": {
+                    do {
+                        scanner = new Scanner(System.in);
+                        System.out.print("\nEnter deposit amount in $");
+                        //validate user input amount
+                        if (scanner.hasNextDouble()) {
+                            double depositAmount = scanner.nextDouble();
+                            account.deposit(depositAmount);
+                            break;
+                        } else System.out.println("Error: invalid amount");
+                    } while (!scanner.hasNextDouble());
+                }
+                break;
+                //withdrawal
+                case "1": {
+                    boolean b;
+                    do {
+                        scanner = new Scanner(System.in);
+                        System.out.print("\nEnter withdrawal amount in $");
+                        b = scanner.hasNextDouble();
+                        //validate user input amount
+                        if (b) {
+                            double withdrawAmount = scanner.nextDouble();
+                            if (withdrawAmount > account.getBalance()) {
+                                System.out.println("Error: withdrawal amount too high. Overdraft not allowed.");
+                                b = false;
+                            } else {
+                                account.withdraw(withdrawAmount);
+                                break;
+                            }
+                        } else System.out.println("Error: invalid amount");
+                    } while (!b);
+                }
+                break;
+                //quit
+                case "2": break;
+                //invalid if user input is not 0, 1, or 2
+                default: System.out.println("Error: invalid input");
+                break;
+            }
+        }
+        //display new balance & goodbye message
+        System.out.print("\nNew balance is $" + account.getBalance() +
+                "\n\nGoodbye!");
+    }
+}
